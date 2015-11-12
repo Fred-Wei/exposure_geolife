e776abc99f9d90714a5cca60229c1616f2c150e1
Guixing Wei <g_w38@txstate.edu>
2015-11-11 22:03:03 -0600

    minor changes

--- mobility_pattern.py
+++ mobility_pattern.py
@@ -13,12 +13,19 @@ from sklearn import cluster
 class activity(gps_dump):
 
     def __init__(self):
-        gps_dump.__init__()
+        gps_dump.__init__(self)
+        gps_dump.conn()
+        self.nop = 0
+        self.nou=1 # for now the # of users is only 1
 
+    def calc_nop(self):#determine the #mini for cluster of NBSC algorithm
+        self.cur.execute(r'select count(*) from geolife.gps_points where uid=0 and day_week<5')
+        rows = self.cur.fetchall()
+        if self.nop == 0:
+            self.nop = rows
 
     def find_home(self):
-        self.num_uid()
-        for i in range(0,self.num_users,1):
+        for i in range(0,self.nou,1):
             try:
                 self.cur.execute("SELECT * FROM geolife.gps_points WHERE uid= {0} "
                                  "and hour_day>'02:00:00' and hour_day<'04:00:00' "
@@ -31,7 +38,4 @@ class activity(gps_dump):
                 print err.message
 
 
-
-
 if __name__ =="__main__":
-    pass
